c1bc6134a15c2f1d9a33132916906f4d0a4d80c1
adecesa1 <adecesa1@cs3202-vm08.uwg.westga.edu>
2023-04-18 16:18:09 -0400

    Revert "Revert commit"
    This reverts commit a20c6956a7819282e8270feeb832199c9ddc0ab3.

---
+++ src/view/GameWindow.h
@@ -0,0 +1,40 @@
+/*
+ * GameWindow.h
+ *
+ *  Created on: Apr 13, 2023
+ *      Author: cwill124
+ */
+
+#ifndef VIEW_GAMEWINDOW_H_
+#define VIEW_GAMEWINDOW_H_
+#include <Fl/Fl_Input.H>
+#include <Fl/Fl_Box.H>
+#include <Fl/Fl_Button.H>
+#include "OKCancelWindow.h"
+#include "MainWindow.h"
+#include <vector>
+#include <iostream>
+#include <regex>
+using namespace std;
+namespace view {
+
+class GameWindow: public OKCancelWindow {
+private:
+	vector<Fl_Input*> inputBoxes;
+	Fl_Box* puzzleLevel;
+	Fl_Button* resetButton;
+	static void cb_getValue(Fl_Widget*, void* data);
+	void createBoxes();
+	bool checkOtherInputValues(Fl_Widget* widget );
+	static void cb_resetBoard(Fl_Widget* widget, void* data);
+
+public:
+	GameWindow();
+	void okHandler();
+	void cancelHandler();
+	virtual ~GameWindow();
+};
+
+} /* namespace view */
+
+#endif /* VIEW_GAMEWINDOW_H_ */
