4d2e5b122f7df56745961710a4ef3d783b6a813e
cwill124 <cwill124@cs3202-vm16.uwg.westga.edu>
2023-05-01 14:23:28 -0400

    fixed the overlapping of the messages

--- src/view/GameWindow.cpp
+++ src/view/GameWindow.cpp
@@ -110,6 +110,7 @@ void GameWindow::createBoxes() {
 void GameWindow::cb_getValue(Fl_Widget *widget, void *data) {
 	Fl_Input *input = (Fl_Input*) widget;
 	GameWindow *window = (GameWindow*) data;
+	window->gameOutcomeLabel->label("");
 	const char *value = input->value();
 	regex pattern("^[^a-zA-Z]*$");
 	regex patternNumbers("([1-5]?[1-9]|6[0-4])");
